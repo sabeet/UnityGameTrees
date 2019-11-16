50678c013b2ea49f5f8a43f60a47874610ae2d10
Sabeet <sabeet.other.way@gmail.com>
2019-11-16 04:50:22 -0500

    Added files

---
+++ New Unity Project/Assets/playerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour{
+
+    public Rigidbody rb;
+    public float forwardForce = 10f;
+    public float sidewaysForce = 2.5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,-forwardForce*Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(-sidewaysForce*Time.deltaTime,0,0);
+        }
+        else if(Input.GetKey("a")){
+            rb.AddForce(sidewaysForce*Time.deltaTime,0,0);
+        }
+    }
+}
